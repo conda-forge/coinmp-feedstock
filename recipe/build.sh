b382f5348f7b526d4db4ef316ab6c7bf04d671e4
David Lecomber <david.lecomber@arm.com>
2025-01-21 18:00:02 +0000

    Copy new config.* to all subdirectories that run ./configure ;
    as the stale pre-existing contents does not recognize aarch64.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 # fix stale config.guess/config.sub to enable linux-aarch64 etc.
+
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
+cp $BUILD_PREFIX/share/gnuconfig/config.* Osi
+cp $BUILD_PREFIX/share/gnuconfig/config.* Clp
+cp $BUILD_PREFIX/share/gnuconfig/config.* Cgl
+cp $BUILD_PREFIX/share/gnuconfig/config.* Cbc
+cp $BUILD_PREFIX/share/gnuconfig/config.* CoinMP
+cp $BUILD_PREFIX/share/gnuconfig/config.* Data/Sample
+cp $BUILD_PREFIX/share/gnuconfig/config.* CoinUtils
+cp $BUILD_PREFIX/share/gnuconfig/config.* BuildTools
+
 
 sh ./configure --prefix=$PREFIX --enable-static --enable-gnu-packages
 chmod +x ./CoinUtils/install-sh
